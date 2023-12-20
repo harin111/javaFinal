554df5e9c88d6a626104fc3a442e6b518c1eb18a
harin <infenityasd@gmail.com>
2023-12-20 18:40:44 +0700

    log in
    sdf

--- JavaFn/src/main/java/com/tdtu/JavaFn/JavaFnApplication.java
+++ JavaFn/src/main/java/com/tdtu/JavaFn/JavaFnApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.tdtu.JavaFn")
 public class JavaFnApplication {
-
+//sdfkjsdflksjgperrjptfoskdr
 	public static void main(String[] args) {
 		SpringApplication.run(JavaFnApplication.class, args);
 	}
